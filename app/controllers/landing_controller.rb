adddf577f70e099d9244a8d92894c3c1d236eb46
アリフ　マウラナ <a_maulana@sbs-infosys.co.jp>
2021-12-01 15:23:28 +0900

    login page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,6 @@
 class LandingController < ApplicationController
+  before_action :authenticate_user!
+  
   def home
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(page_title='')
+        base_title = "SBS検定問題"
+        if page_title.empty?
+          base_title
+        else
+          page_title+"|"+base_title
+        end
+      end
 end
